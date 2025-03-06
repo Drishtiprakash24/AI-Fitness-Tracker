fa0719812690c1fc39823191326ce492988f22a6
Drishtiprakash24 <drishtiprakash24@gmail.com>
2025-03-07 01:30:31 +0530

    Updated Code

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 import streamlit as st
 import sqlite3
 import pandas as pd
-from functions import register_user,log_workout
+import matplotlib.pyplot as plt
+from functions import register_user,log_workout,calculate_bmi
 
 conn=sqlite3.connect("fitness_tracker.db",check_same_thread=False)
 cursor=conn.cursor()
 
 st.title("Personal Fitness Tracker 🏋️")
+st.write("In this WebApp you will be able to observe your predicted calories burned in your body. Pass your parameters such as `Age`, `Gender`, `BMI`, etc., into this WebApp and then you will see the predicted value of kilocalories burned.")
 
 st.header("Register User")
 name=st.text_input("Name :")
@@ -25,7 +27,18 @@ if st.button("Save Workout"):
     st.success(log_workout(user_id,exercise,duration,calories))
 
 st.header("View Progress")
+if st.button("Calculate BMI"):
+    st.write("Your BMI is:", calculate_bmi(weight, height))
+
 if st.button("Show Progress"):
-    df=pd.read_sql_query(f"SELECT date,calories FROM workouts WHERE user_id={user_id}",conn)
-    st.line_chart(df.set_index("date"))
+    
+    df = pd.read_sql_query(f"SELECT date, calories FROM workouts WHERE user_id={user_id}", conn)
+    conn.close()
+    fig,ax=plt.subplots(figsize=(8,5))
+    ax.plot(df['date'],df['calories'],marker="o",linestyle='-')
+    ax.set_xlabel("Date")
+    ax.set_ylabel("Calories Burned")
+    ax.set_title("Workout Progress")
+    ax.tick_params(axis='x',rotation=45)
+    st.pyplot(fig)
     
